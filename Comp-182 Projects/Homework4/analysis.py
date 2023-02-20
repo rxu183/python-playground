edba1d10cc5c19fbf6b0b8d48d4e53008425016a
Richard <rgx1@rice.edu>
2023-02-19 21:13:15 -0600

    Initial

---
+++ Comp-182 Projects/Homework4/analysis.py
@@ -0,0 +1,9 @@
+# Richard Xu
+# rgx1
+# COMP 182 Spring 2023 - Homework 4, Problem 3
+
+# You can import any standard library, as well as Numpy and Matplotlib.
+# You can use helper functions from comp182.py, provided.py, and autograder.py,
+# but they have to be copied over here.
+
+# Your code here...
---
+++ Comp-182 Projects/Homework4/autograder.py
@@ -0,0 +1,13 @@
+# Richard Xu
+# rgx1
+# COMP 182 Spring 2023 - Homework 4, Problem 3
+
+# You may NOT import anything apart from already imported libraries.
+# You can use helper functions from comp182.py and provided.py, but they have
+# to be copied over here.
+
+from collections import *
+
+def compute_largest_cc_size(g: dict) -> int:
+    # Your code here...
+    return 0 
